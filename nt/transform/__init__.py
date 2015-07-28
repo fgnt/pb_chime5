77c751b980c93c378cc9f6f9f3bb6ed086c90f46
Lukas Drude <drude@nt.uni-paderborn.de>
2015-07-28 13:42:42 +0200

    Jahn's start to correct import statements

--- nt/transform/__init__.py
+++ nt/transform/__init__.py
@@ -1,19 +1,25 @@
 """
 This module deals with all sorts of audio input and output.
 """
-from nt.transform import fbank, filter, mfcc, ssc, stft
 
-fbank = fbank.fbank
+from nt.transform import filter
+from nt.transform import fbank
+# from nt.transform import mfcc
+# from nt.transform import ssc
+from nt.transform import mod_stft
 
-preemphasis = filter.preemphasis
-
-mfcc = mfcc.mfcc
-
-ssc = ssc.ssc
-
-stft = stft.stft
-istft = stft.istft
-stft_to_spectrogram = stft.stft_to_spectrogram
-plot_spectrogram = stft.plot_spectrogram
-plot_stft = stft.plot_stft
-spectrogram_to_energy_per_frame = stft.spectrogram_to_energy_per_frame
+# filter = filter
+# fbank = fbank.fbank
+#
+# preemphasis = filter.preemphasis
+#
+# mfcc = mfcc.mfcc
+#
+# ssc = ssc.ssc
+#
+stft = mod_stft.stft
+# istft = stft.istft
+# stft_to_spectrogram = stft.stft_to_spectrogram
+# plot_spectrogram = stft.plot_spectrogram
+# plot_stft = stft.plot_stft
+# spectrogram_to_energy_per_frame = stft.spectrogram_to_energy_per_frame
