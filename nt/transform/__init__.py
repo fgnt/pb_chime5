a18fd477ce33a324783ccf7e4384c374f2c9b7e0
Lukas Drude <drude@nt.uni-paderborn.de>
2015-07-28 15:08:04 +0200

    __init__py is now much flatter.

--- nt/transform/__init__.py
+++ nt/transform/__init__.py
@@ -1,27 +1,14 @@
 """
 This module deals with all sorts of acoustic features and transforms.
 """
-
-#from nt.transform import filter
-#from nt.transform import fbank
-# from nt.transform import mfcc
-# from nt.transform import ssc
-#from nt.transform import stft
-
-# filter = filter
-# fbank = fbank.fbank
-#
-# preemphasis = filter.preemphasis
-#
-# mfcc = mfcc.mfcc
-#
-# ssc = ssc.ssc
-#
-#istft = nt.transform.stft.istft
-#stft_to_spectrogram = nt.transform.stft.stft_to_spectrogram
-#plot_spectrogram = nt.transform.stft.plot_spectrogram
-#plot_stft = nt.transform.stft.plot_stft
-#spectrogram_to_energy_per_frame = nt.transform.stft.spectrogram_to_energy_per_frame
-#stft2 = gespenst.stft
-
-#stft_module = stft
+from nt.transform.module_stft import stft
+from nt.transform.module_stft import istft
+from nt.transform.module_stft import stft_to_spectrogram
+from nt.transform.module_stft import plot_spectrogram
+from nt.transform.module_stft import plot_stft
+from nt.transform.module_stft import spectrogram_to_energy_per_frame
+from nt.transform.module_filter import preemphasis
+from nt.transform.module_fbank import fbank
+from nt.transform.module_filter import preemphasis
+from nt.transform.module_mfcc import mfcc
+from nt.transform.module_ssc import ssc
