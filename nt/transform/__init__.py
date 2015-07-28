814cd1f5a98aa060969db1cb5c8fdc9c05ff8a26
Lukas Drude <drude@nt.uni-paderborn.de>
2015-07-28 10:42:53 +0200

    Expose most transform functions

--- nt/transform/__init__.py
+++ nt/transform/__init__.py
@@ -1,4 +1,19 @@
 """
 This module deals with all sorts of audio input and output.
 """
-from nt.transform import stft
+from nt.transform import fbank, filter, mfcc, ssc, stft
+
+fbank = fbank.fbank
+
+preemphasis = filter.preemphasis
+
+mfcc = mfcc.mfcc
+
+ssc = ssc.ssc
+
+stft = stft.stft
+istft = stft.istft
+stft_to_spectrogram = stft.stft_to_spectrogram
+plot_spectrogram = stft.plot_spectrogram
+plot_stft = stft.plot_stft
+spectrogram_to_energy_per_frame = stft.spectrogram_to_energy_per_frame
