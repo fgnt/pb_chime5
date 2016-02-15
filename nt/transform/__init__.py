331ed8476bf2263ac94248a175268209fd63895b
Lukas Drude <mail@lukas-drude.de>
2016-02-15 14:50:12 +0100

    Added inverse preemphasis filter

--- nt/transform/__init__.py
+++ nt/transform/__init__.py
@@ -6,7 +6,8 @@ from nt.transform.module_stft import istft
 from nt.transform.module_stft import stft_to_spectrogram
 from nt.transform.module_stft import spectrogram_to_energy_per_frame
 from nt.transform.module_filter import preemphasis
+from nt.transform.module_filter import inverse_preemphasis
 from nt.transform.module_fbank import fbank
-from nt.transform.module_filter import offcomp
+from nt.transform.module_filter import offset_compensation
 from nt.transform.module_mfcc import mfcc
 from nt.transform.module_ssc import ssc
