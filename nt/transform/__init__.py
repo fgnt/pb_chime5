1a7148601bc4f4f32927b07ec36c72e8ec45c36a
Lukas Drude <mail@lukas-drude.de>
2016-04-04 14:35:11 +0200

    Added spectrogram, added velocity and acc for mfcc, added normalize

--- nt/transform/__init__.py
+++ nt/transform/__init__.py
@@ -1,7 +1,7 @@
 """
 This module deals with all sorts of acoustic features and transforms.
 """
-from nt.transform.module_stft import stft
+from nt.transform.module_stft import stft, spectrogram
 from nt.transform.module_stft import istft
 from nt.transform.module_stft import stft_to_spectrogram
 from nt.transform.module_stft import spectrogram_to_energy_per_frame
@@ -10,5 +10,17 @@ from nt.transform.module_filter import preemphasis
 from nt.transform.module_filter import inverse_preemphasis
 from nt.transform.module_fbank import fbank
 from nt.transform.module_filter import offset_compensation
-from nt.transform.module_mfcc import mfcc
+from nt.transform.module_mfcc import mfcc, mfcc_velocity_acceleration
 from nt.transform.module_ssc import ssc
+import numpy as np
+
+
+def normalize_mean_variance(data, axis=0, eps=1e-6):
+    """ Normalize features.
+
+    :param data: Any feature
+    :param axis: Time dimensions, default is 0
+    :return: Normalized observation
+    """
+    return (data - np.mean(data, axis=axis, keepdims=True)) /\
+        (np.std(data, axis=axis, keepdims=True) + eps)
