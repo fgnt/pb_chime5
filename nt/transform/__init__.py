54fcf850af42949127910cff756daab09f923774
Jahn Heymann <heymann@nt.upb.de>
2016-05-03 15:32:47 +0200

    Speed-Up

--- nt/transform/__init__.py
+++ nt/transform/__init__.py
@@ -10,6 +10,7 @@ from nt.transform.module_filter import preemphasis
 from nt.transform.module_filter import inverse_preemphasis
 from nt.transform.module_fbank import fbank
 from nt.transform.module_filter import offset_compensation
+from nt.transform.module_filter import preemphasis_with_offset_compensation
 from nt.transform.module_mfcc import mfcc, mfcc_velocity_acceleration
 from nt.transform.module_ssc import ssc
 from nt.transform.module_bark_fbank import bark_fbank
