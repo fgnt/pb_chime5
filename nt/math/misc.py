ae40a01e35a1a26bff92b40aa3a453d418c89d1d
Jahn Heymann <jheymann@mail.uni-paderborn.de>
2016-11-25 16:08:37 +0100

    Refactored almost everything. Will break almost everything :)

---
+++ nt/math/misc.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def softmax(x, feature_axis=-1):
+    """ Calculates the softmax activation
+
+    :param x: Input signal
+    :param feature_axis: Dimension holding the features to apply softmax on
+    :return: Softmax features
+    """
+    net_out_e = x - x.max(axis=feature_axis, keepdims=True)
+    np.exp(net_out_e, out=net_out_e)
+    net_out_e /= net_out_e.sum(axis=feature_axis, keepdims=True)
+    return net_out_e
