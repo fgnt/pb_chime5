cd0066e52e725719b6240cb2686dbb9b95a86c4e
Lukas Drude <ldrude@mail.uni-paderborn.de>
2016-11-25 21:19:33 +0100

    Enforcement of pathlib.Path or string for json functions. If you want file pointers, use json.load directly.

--- nt/io/__init__.py
+++ nt/io/__init__.py
@@ -4,8 +4,8 @@ This module deals with all sorts of input and output.
 There is special focus on audio, but there are also some convenience imports
 i.e. for load_json() and similar functions.
 
-The file path is called `path` just as it has been done in audioread.
-
+The file path is called `path` just as it has been done in ``audioread``.
+The path should either be a ``pathlib.Path`` object or a string.
 """
 from pathlib import Path
 
@@ -16,29 +16,8 @@ from nt.io.json import load_json, dump_json
 
 __all__ = [
     'load_json', 'dump_json',
-    'load_hdf5', 'dump_hdf5'
+    'load_hdf5', 'dump_hdf5', 'update_hdf5'
 ]
-
-#
-# def load_json(path, internal_path='/'):
-#     assert isinstance(path, (str, Path))
-#     assert isinstance(internal_path, (str, Path)) or internal_path is None
-#
-#     with open(path) as fid:
-#         content = json.load(fid)
-#
-#     internal_path = str(internal_path)
-#     for key in internal_path.strip('/').split('/')[1:]:
-#         content = content[key]
-#
-#     return content
-#
-#
-# def dump_json(data, path):
-#     assert isinstance(path, (str, Path))
-#
-#     with open(path, 'w') as f:
-#         json.dump(data, f, indent=2, cls=_Encoder)
 
 
 def load_hdf5(path, internal_path='/'):
@@ -55,6 +34,7 @@ def dump_hdf5(data, path):
         data,
         str(path)
     )
+
 
 def update_hdf5(data, path):
     assert isinstance(path, (str, Path))
