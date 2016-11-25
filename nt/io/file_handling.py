df9d5c2d53c315e85eecb3bdce719b9fb40a36d9
Jahn Heymann <jheymann@mail.uni-paderborn.de>
2016-11-25 16:08:37 +0100

    Refactored almost everything. Will break almost everything :)

---
+++ nt/io/file_handling.py
@@ -0,0 +1,39 @@
+from contextlib import contextmanager
+import os
+
+
+def mkdir_p(path):
+    """ Creates a path recursively without throwing an error if it already exists
+
+    :param path: path to create
+    :return: None
+    """
+    try:
+        os.makedirs(path)
+    except FileExistsError:
+        pass
+    except FileNotFoundError:
+        if path == '':
+            pass
+
+
+@contextmanager
+def change_directory(new_path):
+    """ Context manager for changing the current working directory.
+
+    Will return to original directory even on error.
+
+    http://stackoverflow.com/a/24176022/911441
+
+    Args:
+        new_path: Directory to change to.
+
+    Returns:
+
+    """
+    saved_path = os.getcwd()
+    os.chdir(os.path.expanduser(new_path))
+    try:
+        yield
+    finally:
+        os.chdir(saved_path)
