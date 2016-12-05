03ee154a04e6aa2d8d4a5613a6eb21b02c1d8a86
Christoph <cbj@mail.uni-paderborn.de>
2016-12-05 17:14:06 +0100

    replace custom DataDir class with builtin pathlib.Path

--- nt/io/data_dir.py
+++ nt/io/data_dir.py
@@ -1,65 +1,43 @@
+"""
+
+>>> from pathlib import Path
+>>> p = Path('/') / 'net' # /storage/python_unittest_data
+>>> p
+>>> p = p / 'storage'
+>>> p
+>>> str(p)
+
+"""
+
 import os
+from pathlib import Path
 
 
-class DataDir(str):
-    """
-    Append string class with join, for easier calling.
-
-    Example:
-
-        >>> import os
-        >>> os.environ['NT_TESTING_DIR'] = '/net'
-        >>> from nt.io.data_dir import testing as data_dir
-        >>> data_dir.join('a', 'b')
-        '/net/a/b'
-        >>> data_dir
-        '/net'
-        >>> data_dir / 'b' / 'c'
-        '/net/b/c'
-        >>> os.path.join(data_dir, '2')
-        '/net/2'
-        >>> data_dir('2')
-        '/net/2'
-        >>> type(data_dir.join('a', 'b'))
-        <class 'str'>
-    """
-
-    def join(self, *args):
-        return os.path.join(self, *args)
-
-    def __call__(self, *args):
-        return os.path.join(self, *args)
-
-    def __div__(self, other):
-        return DataDir(os.path.join(self, other))
-    __truediv__ = __div__
-
-
-testing = DataDir(os.getenv(
+testing = Path(os.getenv(
     'NT_TESTING_DIR',
     '/net/storage/python_unittest_data'
 ))
-timit = DataDir(os.getenv(
+timit = Path(os.getenv(
     'NT_TIMIT_DIR',
     '/net/speechdb/timit'
 ))
-tidigits = DataDir(os.getenv(
+tidigits = Path(os.getenv(
     'NT_TIDIGITS_DIR',
     '/net/speechdb/tidigits'
 ))
-database_jsons = DataDir(os.getenv(
+database_jsons = Path(os.getenv(
     'NT_DATABASE_JSONS_DIR',
     '/net/storage/database_jsons'
 ))
-chime = DataDir(os.getenv(
+chime = Path(os.getenv(
     'NT_CHIME_DIR',
     '/net/ssd/2015/chime'
 ))
-
-kaldi_root = DataDir(os.getenv(
+kaldi_root = Path(os.getenv(
     'KALDI_ROOT',
     '/net/ssd/software/kaldi'
 ))
+
 
 if __name__ == "__main__":
     import doctest
