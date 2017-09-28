5e9440ddf405b8abded608e88e122ba667ee3c59
Christoph <cbj@mail.uni-paderborn.de>
2017-09-28 16:38:59 +0200

    run_processes: - deprecated sleep_time - improved exception

--- nt/utils/process_caller.py
+++ nt/utils/process_caller.py
@@ -7,13 +7,16 @@ import os
 DEBUG_MODE = False
 DEFAULT_ENV = os.environ.copy()
 
-def run_processes(cmds, sleep_time=0.1, ignore_return_code=False,
+
+def run_processes(cmds, sleep_time=None, ignore_return_code=False,
                   environment=DEFAULT_ENV, warn_on_ignore=True,
                   inputs=None, *, cwd=None, shell=True):
     """ Starts multiple processes, waits and returns the outputs when available
 
     :param cmd: A list with the commands to call
     :param sleep_time: Intervall to poll the running processes (in seconds)
+        This option is deprecated, the implementation does not need a
+        sleep_time anymore.
     :param ignore_return_code: If true, ignores non zero return codes.
         Otherwise an exception is thrown.
     :param environment: environment (e.g. path variable) for commands
@@ -22,6 +25,11 @@ def run_processes(cmds, sleep_time=0.1, ignore_return_code=False,
     :return: Stdout, Stderr and return code for each process
     """
     # ToDo: remove sleep_time
+    if sleep_time is not None:
+        import warnings
+        warnings.warn('Call nt.utils.process_caller.run_processes(..., '
+                      'sleep_time={}, ...) is deprecated. sleep_time will be '
+                      'removed in the future'.format(sleep_time))
 
     # Ensure its a list if a single command is passed
     cmds = cmds if isinstance(cmds, list) else [cmds]
@@ -32,14 +40,19 @@ def run_processes(cmds, sleep_time=0.1, ignore_return_code=False,
 
     if DEBUG_MODE:
         [print('Calling: {}'.format(cmd)) for cmd in cmds]
-    pipes = [subprocess.Popen(cmd,
-                              stdin=subprocess.PIPE,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE,
-                              shell=shell,
-                              universal_newlines=True,
-                              cwd=cwd,
-                              env=environment) for cmd in cmds]
+    try:
+        pipes = [subprocess.Popen(cmd,
+                                  stdin=subprocess.PIPE,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE,
+                                  shell=shell,
+                                  universal_newlines=True,
+                                  cwd=cwd,
+                                  env=environment) for cmd in cmds]
+    except FileExistsError:
+        print(cmds)
+        print(cwd)
+        raise
     return_codes = len(cmds) * [None]
     stdout = len(cmds) * [None]
     stderr = len(cmds) * [None]
