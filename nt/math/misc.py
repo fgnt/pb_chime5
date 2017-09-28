0baa1965e3720fdfa07844563a1714bbd59d5804
Christoph <cbj@mail.uni-paderborn.de>
2017-09-28 16:36:58 +0200

    add abs_square for numpy 1.13.1, fix mask_module

--- nt/math/misc.py
+++ nt/math/misc.py
@@ -12,3 +12,28 @@ def softmax(x, feature_axis=-1):
     np.exp(net_out_e, out=net_out_e)
     net_out_e /= net_out_e.sum(axis=feature_axis, keepdims=True)
     return net_out_e
+
+
+def abs_square(x: np.ndarray):
+    """
+
+    https://github.com/numpy/numpy/issues/9679
+
+    Bug in numpy 1.13.1
+    >> np.ones(32768).imag ** 2
+    Traceback (most recent call last):
+    ...
+    ValueError: output array is read-only
+    >> np.ones(32767).imag ** 2
+    array([ 0.,  0.,  0., ...,  0.,  0.,  0.])
+
+    >>> abs_square(np.ones(32768)).shape
+    (32768,)
+    >>> abs_square(np.ones(32768, dtype=np.complex64)).shape
+    (32768,)
+    """
+
+    if np.iscomplexobj(x):
+        return x.real ** 2 + x.imag ** 2
+    else:
+        return x ** 2
