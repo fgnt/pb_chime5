81cf9c4c058e92bf0c638d32dd3a16d5a99d399b
Christoph <cbj@mail.uni-paderborn.de>
2018-01-03 17:12:02 +0100

    add scheduler with sacred

---
+++ cbj/scheduler/mpi/_func_share_master.py
@@ -0,0 +1,82 @@
+if not __package__:
+    from cbj_lib import set_package
+
+    set_package()
+
+from .helper import *
+
+
+def share_master(iterator, disable_pbar=False):
+    """
+    A master process pushes tasks to the workers.
+    Required at least 2 mpi processes, but to produce a speedup 3 are required.
+
+    Parallel: Body of the for loop.
+
+    Note:
+        Inside the for loop a break is allowed to mark the current task as
+        successful and do not calculate further tasks.
+
+
+    ToDo:
+        When a slave throw a exception, the task is currently ignored.
+        Change it that the execution get canceled.
+
+    """
+
+    assert size > 1, size
+    from tqdm import tqdm
+    import itertools
+
+    status = MPI.Status()
+    workers = size - 1
+
+    comm.Barrier()
+
+    if rank == 0:
+        i = 0
+        with tqdm(itertools.count(), total=len(iterator), disable=disable_pbar) as pbar:
+            pbar.set_description(f'busy: {workers}')
+            while workers > 0:
+                source = comm.recv(
+                    source=MPI.ANY_SOURCE,
+                    status=status)
+                if source is not None:
+                    comm.send(i, dest=source)
+                    i += 1
+                    pbar.update()
+                else:
+                    workers -= 1
+                    pbar.set_description(f'busy: {workers}')
+
+        assert workers == 0, workers
+        # i is bigger than len(iterator), because the slave says value is to big
+        # and than the master increases the value
+        assert len(iterator) < i, f'{len(iterator)}, {i}: Iterator is not consumed'
+    else:
+        try:
+            comm.send(rank, dest=0)
+            next_index = comm.recv(source=0)
+            for i, val in enumerate(iterator):
+                if i == next_index:
+                    yield val
+                    comm.send(rank, dest=0)
+                    next_index = comm.recv(source=0)
+        finally:
+            comm.send(None, dest=0)
+
+
+if __name__ == '__main__':
+    from .helper import test_relaunch_with_mpi
+    test_relaunch_with_mpi()
+
+    iter = range(5)
+
+    comm.Barrier()
+
+    for i in share_master(iter, disable_pbar=True):
+        print('loop body:', i, rank)
+        if rank == 2:
+            break
+
+    print('Exit', rank)
