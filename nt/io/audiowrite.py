d6b7c311f0e25dcff4d0c21064596165752baca7
Christoph <cbj@mail.uni-paderborn.de>
2018-01-16 17:49:52 +0100

    audiowrite: improved exception

--- nt/io/audiowrite.py
+++ nt/io/audiowrite.py
@@ -52,11 +52,13 @@ def audiowrite(data, path, sample_rate=16000, normalize=False, threaded=True):
     else:
         try:
             wav_write(path, sample_rate, data)
-        except Exception:  #_struct.error
+        except Exception:  # _struct.error
             if data.ndim == 2:
-                assert data.shape[1] < 20, \
-                    "channels bigger than 20 looks wrong. " \
-                    "Maybe you must call audiowrite(data.T, ...)"
+                assert data.shape[1] < 20, (
+                    f"channels bigger than 20 looks wrong "
+                    f"(shape: {data.shape}). "
+                    f"Maybe you must call audiowrite(data.T, ...)"
+                )
             raise
 
     return sample_to_clip
