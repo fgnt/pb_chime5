f7e446395245cad4a8a66e66cfce5d5583db227d
Jahn Heymann <heymann@nt.upb.de>
2018-04-02 12:47:52 +0200

    Improves checking of already decoded files Returns NaN instead of 100 on error

--- nt/kaldi/decode.py
+++ nt/kaldi/decode.py
@@ -7,6 +7,8 @@ import warnings
 import pandas
 import pickle
 import logging
+import glob
+import numpy as np
 
 
 def _build_rescale_lattice_cmd(decode_dir, hclg_dir, lmwt,
@@ -47,7 +49,7 @@ def _parse_wer_lines(lines):
             lines).groups()
         return float(wer), int(errors), int(words), int(ins), int(del_), int(
             sub)
-    return 100, 0, 0, 0, 0, 0
+    return np.nan, 0, 0, 0, 0, 0
 
 
 def parse_wer_file(wer_file):
@@ -57,10 +59,12 @@ def parse_wer_file(wer_file):
             return _parse_wer_lines(lines)
     except Exception as e:
         warnings.warn('Exception during parsing of WER file: {}'.format(e))
-    return 100, 0, 0, 0, 0, 0
+    return np.nan, 0, 0, 0, 0, 0
 
 
 def _tra_complete(tra_file, ref_file):
+    if not os.path.exists(tra_file):
+        return False
     with open(tra_file) as fid:
         tra = set([l.split()[0] for l in fid.readlines()])
     with open(ref_file) as fid:
@@ -70,10 +74,30 @@ def _tra_complete(tra_file, ref_file):
         return True
     else:
         logging.getLogger('_tra_complete').warn(
-            f'{tra_file} is missing the {len(diff)} utts. '
+            f'{tra_file} is missing {len(diff)} utts. '
             f'Samples: {list(diff)[:min(len(diff), 5)]}'
         )
     return False
+
+
+def _lattices_exists(ref_file, lat_dir):
+    with open(ref_file) as fid:
+        ref = set([l.split()[0] for l in fid.readlines()])
+    lat_files = set(
+        l.split('/')[-1].replace('.lat', '')
+        for l in glob.glob(f'{lat_dir}/*.lat')
+    )
+    diff = ref - lat_files
+    if not len(diff):
+        return True
+    else:
+        logging.getLogger('_lattices_exists').warn(
+            f'{lat_dir} is missing {len(diff)} utts.\n'
+            f'Samples: {list(diff)[:min(len(diff), 5)]}\n'
+            f'Files: {list(lat_files)[:min(len(lat_files), 5)]}'
+        )
+    return False
+
 
 def compute_scores(decode_dir, hclg_dir, ref_text, min_lmwt=8, max_lmwt=18,
                    force_scoring=False, build_tra=True, strict=True,
@@ -89,14 +113,19 @@ def compute_scores(decode_dir, hclg_dir, ref_text, min_lmwt=8, max_lmwt=18,
         ignore_return_code=ignore_return_codes
     )
     cmds = list()
+    tra_written = dict()
     for lmwt in range(min_lmwt, max_lmwt + 1):
         tra_file = f'{decode_dir}/scoring/{lmwt}.tra'
-        if (not os.path.exists(tra_file)
-            or force_scoring
-            or not _tra_complete(tra_file, ref_file)) and build_tra:
+        rescale = not os.path.exists(tra_file)
+        rescale &= not _tra_complete(tra_file, ref_file)
+        rescale &= _lattices_exists(ref_file, f'{decode_dir}/lats')
+        rescale &= build_tra
+        rescale |= force_scoring
+        if rescale:
             LOG.info(f'Rescaling lattice for lmwt {lmwt}')
             cmds.append(
                 _build_rescale_lattice_cmd(decode_dir, hclg_dir, lmwt))
+            tra_written[lmwt] = True
     if len(cmds):
         helper.excute_kaldi_commands(
             cmds, 'rescaling lattice', log_dir=decode_dir + '/logs',
@@ -106,14 +135,16 @@ def compute_scores(decode_dir, hclg_dir, ref_text, min_lmwt=8, max_lmwt=18,
         LOG.info('All utts already rescaled - skipping')
     cmds = list()
     for lmwt in range(min_lmwt, max_lmwt + 1):
-        LOG.info(f'Computing WER for lmwt {lmwt}')
-        cmds.append(
-            _build_compute_WER_command(
-                decode_dir, hclg_dir, lmwt, strict=strict))
-    helper.excute_kaldi_commands(
-        cmds, 'computing WER', log_dir=decode_dir + '/logs',
-        ignore_return_code=ignore_return_codes
-    )
+        if lmwt in tra_written:
+            LOG.info(f'Computing WER for lmwt {lmwt}')
+            cmds.append(
+                _build_compute_WER_command(
+                    decode_dir, hclg_dir, lmwt, strict=strict))
+    if len(cmds):
+        helper.excute_kaldi_commands(
+            cmds, 'computing WER', log_dir=decode_dir + '/logs',
+            ignore_return_code=ignore_return_codes
+        )
     result = defaultdict(list)
     for lmwt in range(min_lmwt, max_lmwt + 1):
         wer, errors, words, ins, del_, sub = parse_wer_file(
