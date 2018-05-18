9607d14fdca07528ef16bdb85913303912887281
Christoph <cbj@mail.uni-paderborn.de>
2018-05-18 15:14:52 +0200

    share_master: corrected length, disable tqdm thread in example

--- cbj/scheduler/mpi/_func_share_master.py
+++ cbj/scheduler/mpi/_func_share_master.py
@@ -6,7 +6,13 @@ if not __package__:
 from .helper import *
 
 
-def share_master(iterator, disable_pbar=False, allow_single_worker=False):
+def share_master(
+        iterator,
+        # length=None,
+        disable_pbar=False,
+        allow_single_worker=False,
+        pbar_prefix=None,
+):
     """
     A master process pushes tasks to the workers.
     Required at least 2 mpi processes, but to produce a speedup 3 are required.
@@ -37,12 +43,24 @@ def share_master(iterator, disable_pbar=False, allow_single_worker=False):
     status = MPI.Status()
     workers = size - 1
 
+    # print(f'{rank} reached Barrier in share_master')
     comm.Barrier()
+    # print(f'{rank} left Barrier in share_master')
 
     if rank == 0:
         i = 0
-        with tqdm(total=len(iterator), disable=disable_pbar) as pbar:
-            pbar.set_description(f'busy: {workers}')
+        try:
+            length = len(iterator)
+        except Exception:
+            length = None
+
+        if pbar_prefix is None:
+            pbar_prefix = ''
+        else:
+            pbar_prefix = f'{pbar_prefix}, '
+
+        with tqdm(total=length, disable=disable_pbar) as pbar:
+            pbar.set_description(f'{pbar_prefix}busy: {workers}')
             while workers > 0:
                 source = comm.recv(
                     source=MPI.ANY_SOURCE,
@@ -50,15 +68,20 @@ def share_master(iterator, disable_pbar=False, allow_single_worker=False):
                 if source is not None:
                     comm.send(i, dest=source)
                     i += 1
-                    pbar.update()
+                    if length is not None:
+                        if length >= i:
+                            pbar.update()
+                    else:
+                        pbar.update()
                 else:
                     workers -= 1
-                    pbar.set_description(f'busy: {workers}')
+                    pbar.set_description(f'{pbar_prefix}busy: {workers}')
 
         assert workers == 0, workers
         # i is bigger than len(iterator), because the slave says value is to big
         # and than the master increases the value
-        assert len(iterator) < i, f'{len(iterator)}, {i}: Iterator is not consumed'
+        if length is not None:
+            assert length < i, f'{length}, {i}: Iterator is not consumed'
     else:
         try:
             comm.send(rank, dest=0)
@@ -76,13 +99,23 @@ if __name__ == '__main__':
     from .helper import test_relaunch_with_mpi
     test_relaunch_with_mpi()
 
+    import tqdm
+    import time
+    tqdm.tqdm.monitor_interval = 0
+
     iter = range(5)
 
     comm.Barrier()
 
-    for i in share_master(iter, disable_pbar=True):
+    for i in share_master(iter, disable_pbar=False):
         print('loop body:', i, rank)
         if rank == 2:
-            break
+            time.sleep(0.2)
+            # break
+
+    for i in share_master(iter, disable_pbar=False):
+        print('2 loop body:', i, rank)
+        # if rank == 1:
+        #     break
 
     print('Exit', rank)
