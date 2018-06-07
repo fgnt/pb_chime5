05f32aa957d965401c61ed1e06b3ddb608539b11
Christoph <cbj@mail.uni-paderborn.de>
2018-06-07 15:27:22 +0200

    add reduce argument to morph

--- nt/utils/numpy_utils.py
+++ nt/utils/numpy_utils.py
@@ -2,6 +2,7 @@ import warnings
 import numpy as np
 import collections
 import numbers
+from numpy.core.einsumfunc import _parse_einsum_input
 
 
 def segment_axis_v2(x, length: int, shift: int, axis: int=-1,
@@ -574,7 +575,7 @@ def _expanding_reshape(array, source, target, **shape_hints):
     return array
 
 
-def morph(operation, array, **shape_hints):
+def morph(operation, array, reduce=None, **shape_hints):
     """ This is an experimental version of a generalized reshape.
     See test cases for examples.
     """
@@ -593,10 +594,22 @@ def morph(operation, array, **shape_hints):
     # Transpose
     transposition_operation = operation.replace('1', ' ').replace('*', ' ')
     try:
-        array = np.einsum(transposition_operation.replace(' ', ''), array)
+        in_shape, out_shape, (array, ) = _parse_einsum_input([transposition_operation.replace(' ', ''), array])
+
+        if len(set(in_shape) - set(out_shape)) > 0:
+            assert reduce is not None, ('Missing reduce function', reduce, transposition_operation)
+
+            reduce_axis = tuple([i for i, s in enumerate(in_shape) if s not in out_shape])
+            array = reduce(array, axis=reduce_axis)
+            in_shape = ''.join([s for s in in_shape if s in out_shape])
+
+        array = np.einsum(f'{in_shape}->{out_shape}', array)
     except ValueError as e:
-        msg = 'op: {}, shape: {}'.format(transposition_operation,
-                                         np.shape(array))
+        msg = (
+            f'op: {transposition_operation} ({in_shape}->{out_shape}), '
+            f'shape: {np.shape(array)}'
+        )
+
         if len(e.args) == 1:
             e.args = (e.args[0] + '\n\n' + msg,)
         else:
