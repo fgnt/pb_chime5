0a16d501b9a4f4aa8d380bde426cd1492658a490
Christoph <cbj@mail.uni-paderborn.de>
2018-10-10 11:11:52 +0200

    nt/io/data_dir patch did not work. This commit fix the patch

--- nt/io/data_dir.py
+++ nt/io/data_dir.py
@@ -12,100 +12,127 @@
 import os
 from pathlib import Path
 
-database_jsons = Path(os.getenv(
+
+def _get_path(environment_name, default):
+    return Path(os.getenv(environment_name, default)).expanduser()
+
+
+database_jsons = _get_path(
     'NT_DATABASE_JSONS_DIR',
-    '/net/storage/database_jsons'
-))
-db_dir = Path(os.getenv(
+    '/net/vol/jenkins/jsons'
+)
+db_dir = _get_path(
     'NT_DB_DIR',
     '/net/db'
-))
-testing = Path(os.getenv(
+)
+fast_db_dir = _get_path(
+    'NT_FAST_DB_DIR',
+    '/net/fastdb'
+)
+testing = _get_path(
     'NT_TESTING_DIR',
     '/net/storage/python_unittest_data'
-))
-kaldi_root = Path(os.getenv(
+)
+kaldi_root = _get_path(
     'KALDI_ROOT',
-    '/net/ssd/jheymann/software/kaldi_latest'
-    # '/net/ssd/software/kaldi'
-))
-matlab_toolbox = Path(os.getenv(
+    '/net/vol/jenkins/kaldi/2018-01-10_15-43-29_a0b71317df1035bd3c6fa49a2b6bb33c801b56ac')
+matlab_toolbox = _get_path(
     'MATLAB_TOOLBOX_DIR',
     '/net/ssd/software/matlab_toolbox'
-))
-matlab_r2015a = Path(os.getenv(
+)
+matlab_r2015a = _get_path(
     'MATLAB_R2015a',
     '/net/ssd/software/MATLAB/R2015a'
-))
-matlab_license = Path(os.getenv(
+)
+matlab_license = _get_path(
     'MATLAB_LICENSE',
     '/opt/MATLAB/R2016b_studis/licenses/network.lic'
-))
+)
 
-ami = Path(os.getenv(
+ami = _get_path(
     'NT_AMI_DIR',
     db_dir / 'ami'
-))
-timit = Path(os.getenv(
+)
+audioset = _get_path(
+    'NT_AUDIOSET_DIR',
+    fast_db_dir / 'AudioSet'
+)
+dcase_2017_task_3 = _get_path(
+    'NT_DCASE_2017_TASK_3_DIR',
+    fast_db_dir / 'DCASE2017' / 'Task3'
+)
+dcase_2017_task_4 = _get_path(
+    'NT_DCASE_2017_TASK_4_DIR',
+    fast_db_dir / 'DCASE2017' / 'Task4'
+)
+timit = _get_path(
     'NT_TIMIT_DIR',
-    '/net/db/timit'
-))
-tidigits = Path(os.getenv(
+    db_dir / 'timit'
+)
+tidigits = _get_path(
     'NT_TIDIGITS_DIR',
-    '/net/db/tidigits'
-))
-database_jsons = Path(os.getenv(
-    'NT_DATABASE_JSONS_DIR',
-    '/net/storage/database_jsons'
-))
-chime_3 = Path(os.getenv(
+    db_dir / 'tidigits'
+)
+chime_3 = _get_path(
     'NT_CHIME_3_DIR',
-    db_dir / 'chime3'
-))
-chime_4 = Path(os.getenv(
+    fast_db_dir / 'chime3'
+)
+chime_4 = _get_path(
     'NT_CHIME_4_DIR',
-    db_dir / 'chime4'
-))
-merl_mixtures = Path(os.getenv(
+    fast_db_dir / 'chime4'
+)
+chime_5 = _get_path(
+    'NT_CHIME_5_DIR',
+    fast_db_dir / 'chime5'
+)
+merl_mixtures = _get_path(
     'NT_MERL_MIXTURES_DIR',
     '/net/db/merl_speaker_mixtures'
-))
-german_speechdata = Path(os.getenv(
+)
+merl_mixtures_mc = _get_path(
+    'NT_MERL_MIXTURES_MC_DIR',
+    "/net/vol/ldrude/projects/2017/project_dc_storage/merl_mixtures_mc_v6/"
+)
+german_speechdata = _get_path(
     'NT_GERMAN_SPEECHDATA_DIR',
-    '/net/db/german-speechdata-package-v2/'
-))
-noisex92 = Path(os.getenv(
+    '/net/storage/jheymann/speech_db/german-speechdata-package-v2/'
+)
+noisex92 = _get_path(
     'NT_NoiseX_92_DIR',
     db_dir / 'NoiseX_92'
-))
-wsj = Path(os.getenv(
+)
+reverb = _get_path(
+    'NT_REVERB_DIR',
+    fast_db_dir / 'reverb'
+)
+wsj = _get_path(
     'NT_WSJ_DIR',
-    db_dir / 'wsj'
-))
-dcase = Path(os.getenv(
-    'NT_DCASE_DIR',
-    '/home/parora/Documents/DCASE/DCASE 2016/'
-))
-librispeech = Path(os.getenv(
-    'NT_LIBRISPEECH_DIR',
-    '/net/db/LibriSpeech'
-))
-matlab_toolbox = Path(os.getenv(
-    'MATLAB_TOOLBOX_DIR',
-    '/net/ssd/software/matlab_toolbox'
-))
-matlab_r2015a = Path(os.getenv(
-    'MATLAB_R2015a',
-    '/net/ssd/software/MATLAB/R2015a'
-))
-matlab_license = Path(os.getenv(
-    'MATLAB_LICENSE',
-    '/opt/MATLAB/R2016b_studis/licenses/network.lic'
-))
-language_model = Path(os.getenv(
+    fast_db_dir / 'wsj'
+)
+wsj_corrected_paths = _get_path(
+    'NT_WSJ_DIR',
+    db_dir / 'wsj_corrected_paths'
+)
+wsjcam0 = _get_path(
+    'NT_WSJCAM0_DIR',
+    db_dir / 'wsjcam0'
+)
+wsj_bss = _get_path(
+    'NT_WSJ_BSS_DIR',
+    fast_db_dir / 'wsj_bss'
+)
+language_model = _get_path(
     'LANGUAGE_MODEL',
     '/net/storage/jheymann/__share/ldrude/2016/2016-05-10_lm'
-))
+)
+wsj_mc = _get_path(
+    'NT_WSJ_MC_DIR',
+    db_dir / 'wsj_mc_complete'
+)
+librispeech = _get_path(
+    'NT_LIBRISPEECH_DIR',
+    db_dir / 'LibriSpeech'
+)
 
 if __name__ == "__main__":
     import doctest
