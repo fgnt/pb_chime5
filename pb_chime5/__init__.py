5d0aa0af9c57d2d01388b39d51d0054e49f8e91b
Christoph <cbj@mail.uni-paderborn.de>
2018-10-30 17:13:27 +0100

    force single core execution

--- pb_chime5/__init__.py
+++ pb_chime5/__init__.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 
+import os
+os.nice(1)  # be nice
+
+import mkl  # assume numpy from anaconda
+mkl.set_num_threads(1)
+os.environ['OMP_NUM_THREADS'] = str(1)  # recommended for HPC systems
+os.environ['GOMP_NUM_THREADS'] = str(1)  # recommended for HPC (maybe gnu omp)
+os.environ['MKL_NUM_THREADS'] = str(1)
+os.environ['NUMEXPR_NUM_THREADS'] = str(1)
+
 git_root = Path(__file__).parent.parent.resolve().expanduser()
 
 from . import (
