d49b5ddb84d35a28dc73103e55720b20ac5c1616
Christoph <cbj@mail.uni-paderborn.de>
2018-10-30 13:42:28 +0100

    fix mpi share_round_robin

--- pb_chime5/util/mpi/_func_share_round_robin.py
+++ pb_chime5/util/mpi/_func_share_round_robin.py
@@ -3,51 +3,51 @@ if not __package__:
 
     set_package()
 
-from .helper import *
+from . import core as mpi
 
 
 def share_round_robin(iterator, disable_pbar=True):
     """
     Shares the work in round robin fashion
 
-    >>> assert size==2  # mpi size is 2
-    >>> if rank==0:
+    >>> assert mpi.SIZE==2  # mpi size is 2
+    >>> if mpi.RANK==0:
     ...     print(list(share_round_robin(range(5))))
     [0, 2, 4]
-    >>> if rank==1:
+    >>> if mpi.RANK==1:
     ...     print(list(share_round_robin(range(2))))
     [1, 3]
     """
     from itertools import islice
-    if disable_pbar or not ismaster:
-        return islice(iterator, rank, None, size)
+    if disable_pbar or not mpi.IS_MASTER:
+        return islice(iterator, mpi.RANK, None, mpi.SIZE)
     else:
         from tqdm import tqdm
 
         def gen():
             with tqdm(total=len(iterator), desc='MasterPbar') as pbar:
-                for ele in islice(iterator, rank, None, size):
+                for ele in islice(iterator, mpi.RANK, None, mpi.SIZE):
                     yield ele
-                    pbar.update(size)
+                    pbar.update(mpi.SIZE)
 
         return gen()
         # return tqdm(islice(iterator, rank, None, size), desc='MasterPbar')
 
 
 if __name__ == '__main__':
-    from .helper import test_relaunch_with_mpi
-    test_relaunch_with_mpi()
+    # from .helper import test_relaunch_with_mpi
+    # test_relaunch_with_mpi()
 
     it = range(6)
 
-    comm.Barrier()
+    mpi.COMM.Barrier()
 
     for i in share_round_robin(it):
-        print('loop body:', i, rank)
+        print('loop body:', i, mpi.RANK)
 
     # it = range(60)
     # for i in share_round_robin(it, disable_pbar=False):
         # import time
         # time.sleep(0.1)
 
-    print('Exit', rank)
+    print('Exit', mpi.RANK)
