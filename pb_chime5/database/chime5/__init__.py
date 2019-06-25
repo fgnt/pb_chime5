59c3934d1e338e9d3bfd8e248ac61c10969f000b
jensheit <heitkaemper@nt.upb.de>
2019-06-25 17:41:42 +0200

    adds imports to __init__.py

---
+++ pb_chime5/database/chime5/__init__.py
@@ -0,0 +1,10 @@
+from .database import Chime5
+from .database import CHiME5_Keys
+from .database import activity_time_to_frequency
+from .database import activity_frequency_to_time
+
+from pb_chime5.database.chime5.mapping import (
+    session_speakers_mapping,
+    session_dataset_mapping,
+    session_array_to_num_samples_mapping,
+)
