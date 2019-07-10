298811faa6d31ed2868fadfac1c3d0500ba40e50
Christoph <cbj@mail.uni-paderborn.de>
2019-07-10 13:26:20 +0200

    remove tensorflow dependency in setup.py

--- setup.py
+++ setup.py
@@ -23,14 +23,6 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
-try:
-    import tensorflow
-except Exception as e:
-    raise Exception(
-        'The nest module from tensorflow (i.e. tensorflow.python.util.nest) is '
-        'required. Install tensorflow before you install this.'
-    ) from e
-
 setup(
     # This is the name of your project. The first time you publish this
     # package, this name will be registered for you. It will determine how
@@ -148,7 +140,7 @@ setup(
         'cached_property',
         'sacred',
         'nara_wpe',
-	'lazy_dataset',
+    	'lazy_dataset',
     ],  # Optional
 
     # List additional groups of dependencies here (e.g. development
